9d72f314f3a25e0c95852a73755661c44fad883e
LGmedia954 <LeslieGonzalez@me.com>
2022-02-26 13:56:47 -0500

    db troubleshoot

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,5 @@
 # This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
 # Examples:
 #
@@ -11,8 +11,8 @@
 # anaheim = Location.create(city: "Anaheim", state: "California", country: "USA")
 # disney = anaheim.attractions.create(name: "Disneyland", description: "Happiest place on Earth!")
 # disney = Attraction.first
-
-eddie = User.first
-eddie_disney = eddie.trips.create(start_date: "2022-06-01", end_date: "2022-06-08")
-eddie_disney.visits.create(attraction: disney, date: "2022-06-01")
-eddie.comments.create(content: "Fun for all", attraction: disney)
+# eddie = User.first
+# eddie_disney = eddie.trips.create(start_date: "2019-06-01", end_date: "2019-06-08")
+#
+# eddie_disney.visits.create(attraction: disney, date: "2019-06-01")
+# eddie.comments.create(content: "Fun for all", attraction: disney)
