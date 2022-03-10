49e5d2a0da53eea89e0e5f19fb13005c1cc3590c
LGmedia954 <LeslieGonzalez@me.com>
2022-03-09 22:22:19 -0500

    session cookies and credentials

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,9 @@
 class ApplicationController < ActionController::API
-  # include ::ActionController::Cookies
+  include ::ActionController::Cookies
   
   def current_user
-    User.find(session[:user_id])
-    # User.find_by(id: session[:user_id])
+    # User.find(session[:user_id])
+    User.find_by(id: session[:user_id])
   end
  
   def logged_in?
