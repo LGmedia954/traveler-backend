a3b81e53865248b66e7e25722288a51ab5ff903d
LGmedia954 <LeslieGonzalez@me.com>
2022-04-18 03:12:15 -0400

    show method users

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -11,8 +11,11 @@ class Api::V1::UsersController < ApplicationController
   # GET /users/1
   def show
     # render json: @user
+    # json_string = MovieSerializer.new(movie).serialized_json
+    user_json = UserSerializer.new(@user).serialized_json
+    
     # json_string = MovieSerializer.new(movie).serializable_hash.to_json
-    user_json = UserSerializer.new(@user).serializable_hash.to_json
+    # user_json = UserSerializer.new(@user).serializable_hash.to_json
     render json: user_json
   end
 
